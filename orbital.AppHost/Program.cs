ce2927e79720eefc2da4cc740095077cb8d8b2a5
richross <rich.ross@live.com>
2025-04-16 06:59:26 -0400

    end of show

--- orbital.AppHost/Program.cs
+++ orbital.AppHost/Program.cs
@@ -1,4 +1,5 @@
 #pragma warning disable ASPIRECOSMOSDB001
+using Azure.Provisioning.CosmosDB;
 using Microsoft.AspNetCore.Builder;
 using Projects;
 
@@ -8,9 +9,16 @@ var cosmosdb = builder.AddAzureCosmosDB("cosmosdb").RunAsPreviewEmulator(
     emulator =>
     {
         emulator.WithDataExplorer(8888);
-        emulator.WithDataVolume("appData");
+        emulator.WithDataVolume("appData2");
+        //emulator.WithHttpEndpoint(8889);
         emulator.WithLifetime(ContainerLifetime.Persistent);
     });
+
+var database = cosmosdb.AddCosmosDatabase("orbital", "orbital");
+var container = database.AddContainer("container", "/type", "userGroupData");
+
+builder.AddProject<Projects.orbital_api>("orbital-api")
+        .WithReference(cosmosdb);
 
 builder.AddProject<orbital_web>("web");
 
---
+++ orbital.api/Program.cs
@@ -0,0 +1,61 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+//add openAPI support
+
+builder.AddAzureCosmosClient("cosmosdb");
+
+var app = builder.Build();
+
+app.MapDefaultEndpoints();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/api/meetings", () =>
+{
+    return Results.Ok(new
+    {
+        Id = 1,
+        Name = "Meeting 1",
+        Description = "This is a meeting",
+        StartTime = DateTime.UtcNow,
+        EndTime = DateTime.UtcNow.AddHours(1)
+    });
+});
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
