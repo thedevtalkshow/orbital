aacc7766453fbd259617d040e59717464037c3fe
Chris Gomez <cagomez@cellbycell.com>
2025-04-27 23:35:42 -0400

    Set up JSON Serialization to camelCase for NoSQL
    Changed the Meeting data type to C# style capitalization and set the json serialization options to camel case for interaction with Azure Cosmos NoSQL

--- orbital.core/Meeting.cs
+++ orbital.core/Meeting.cs
@@ -8,10 +8,10 @@ namespace orbital.core
 {
     public class Meeting
     {
-        public string id { get; set; }
-        public string type => "meeting";
+        public string Id { get; set; }
+        public string Type => "meeting";
         public string Title { get; set; }
-        public string Description { get; set; }
+        public string Description { get; set; } = string.Empty;
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
     }
