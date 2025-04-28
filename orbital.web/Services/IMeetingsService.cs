7b0b8322b51d9789021c3108f7846a0893cd9b71
Chris Gomez <cagomez@cellbycell.com>
2025-04-28 00:56:27 -0400

    Form to add Meeting and service updates
    Updates to have an ability to add a form to the meeting page.  Added to the Typed HttpClient to call the api.

--- orbital.web/Services/IMeetingsService.cs
+++ orbital.web/Services/IMeetingsService.cs
@@ -5,5 +5,6 @@ namespace orbital.web.Services
     public interface IMeetingsService
     {
         Task<List<Meeting>> GetMeetingsAsync();
+        Task<bool> AddMeetingAsync(Meeting meeting);
     }
 }
